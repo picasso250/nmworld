0f04949136eff95a4e19b263c8c76eea6b6a6d07
xiaochi <wxiaochi@qq.com>
2023-05-07 20:23:03 +0800

    slow

--- game.js
+++ game.js
@@ -25,12 +25,12 @@ function create ()
     role = this.add.image(400, 300, 'role');
 }
 
-function update ()
+function update (time, delta)
 {
     // 随机移动图片
     var angle = Phaser.Math.Angle.Between(0, 0, Phaser.Math.Between(-100, 100), Phaser.Math.Between(-100, 100));
-    var distance = Phaser.Math.Between(5, 20);
-    var vector = new Phaser.Math.Vector2(distance, 0);
+    var distance = Phaser.Math.Between(1, 5);
+    var vector = new Phaser.Math.Vector2(distance * 0.5 * delta / 16.6667, 0); // 16.6667 是 1000 / 60，即每帧的时间
     Phaser.Math.Rotate(vector, angle);
     role.x += vector.x;
     role.y += vector.y;
